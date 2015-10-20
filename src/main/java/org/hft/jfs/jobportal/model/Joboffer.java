e5305e88bce6615f504e232028f103282ff28eaf
Thorsten Kühner <communitiestk@me.com>
2015-10-20 18:21:01 +0200

    reset to previous eclipse project in order to fix Jenkins deployment

---
+++ src/main/java/org/hft/jfs/jobportal/model/Joboffer.java
@@ -0,0 +1,19 @@
+package org.hft.jfs.jobportal.model;
+
+public class Joboffer {
+	private String title;
+	private String description;
+
+	public String getTitle() {
+		return this.title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getDescription() {
+		return this.description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+}
