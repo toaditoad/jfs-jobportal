013249dce50ec1cab20ba3178458cee0fe08cf19
Thorsten Kühner <communitiestk@me.com>
2015-10-20 17:07:23 +0200

    fix

--- JFSGui/web/index.jsp
+++ JFSGui/web/index.jsp
@@ -11,6 +11,6 @@
     <title></title>
   </head>
   <body>
-
+  TEST
   </body>
 </html>
--- JFSService/src/main/java/api/JobofferRest.java
+++ JFSService/src/main/java/api/JobofferRest.java
@@ -7,28 +7,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 
-import org.hft.jfs.jobportal.model.Joboffer;
+import org.hft.jfs.jobportal.model.JobOffer;
 
 @Path("/")
-public class JobofferRest {
+public class JobOfferRest {
 
 	@GET
-	@Path("/joboffer")
+	@Path("/jobOffer")
 	@Produces("application/json")
-	public List<Joboffer> getAllJobOffers() {
-		List<Joboffer> offers = new ArrayList<Joboffer>();
+	public List<JobOffer> getAllJobOffers() {
+		List<JobOffer> offers = new ArrayList<JobOffer>();
 
-		Joboffer offer1 = new Joboffer();
+		JobOffer offer1 = new JobOffer();
 		offer1.setTitle("Bachelor Thesis at Bosch");
 		offer1.setDescription("Write your Bachelor Thesis in the engineering department of Bosch.");
 		offers.add(offer1);
 
-		Joboffer offer2 = new Joboffer();
+		JobOffer offer2 = new JobOffer();
 		offer2.setTitle("Master Thesis at Porsche");
 		offer2.setDescription("Write your Master Thesis in the development department of Porsche.");
 		offers.add(offer2);
 
-		Joboffer offer3 = new Joboffer();
+		JobOffer offer3 = new JobOffer();
 		offer3.setTitle("Summer job");
 		offer3.setDescription("Earn money!");
 		offers.add(offer3);
